ea69b22bb4e90ae02765b47a5ff5543a7800cf3c
Umut Tosun <umut35137@gmail.com>
2023-02-27 19:35:32 +0300

    Fix: code edit

--- js/clock.js
+++ js/clock.js
@@ -1,25 +1,27 @@
 let yourName = prompt("Adınız:");
 let myClock = document.querySelector("#myClock");
-let date = new Date();
-let gunler = [
-  "Pazar",
-  "Pazartesi",
-  "Salı",
-  "Çarşamba",
-  "Perşembe",
-  "Cuma",
-  "Cumartesi",
-];
-if (yourName === "" || yourName === " ") alert("İsminizi Girmediniz");
-else document.querySelector("#myName").innerHTML = yourName;
 
+if (yourName === "" || yourName === " ") {
+  alert("İsminizi Girmediniz");
+} else {
+  document.querySelector("#myName").innerHTML = yourName;
+}
 function currentTime() {
+  let date = new Date();
+  let gunler = [
+    "Pazar",
+    "Pazartesi",
+    "Salı",
+    "Çarşamba",
+    "Perşembe",
+    "Cuma",
+    "Cumartesi",
+  ];
   myClock.innerHTML = `
     ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${
     gunler[date.getDay()]
   } 
 `;
 }
-
 currentTime();
 setInterval(currentTime, 1000);
